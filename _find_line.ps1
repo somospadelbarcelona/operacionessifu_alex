3cf616d0258b5da5af8670a788eec46011f05f0a
Somospadelbarcelona <somospadelbarcelona@gmail.com>
2026-06-18 22:30:18 +0200

    aaaa

--- _find_line.ps1
+++
@@ -1,6 +0,0 @@
-$lines = Get-Content 'app.js'
-for ($i = 0; $i -lt $lines.Length; $i++) {
-    if ($lines[$i] -match 'function refreshMetrics') {
-        Write-Host "LINEA $($i+1): $($lines[$i])"
-    }
-}
